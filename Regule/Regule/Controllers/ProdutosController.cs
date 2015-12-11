d063432977b2005f3ed511757287e8f5b0e14467
Lucas Gabriel <lgfas1@hotmail.com>
2015-12-11 17:22:17 -0300

    Adição de CRUD's Básicos e criação e modificação no banco na pasta seed

---
+++ Regule/Regule/Controllers/ProdutosController.cs
@@ -0,0 +1,120 @@
+﻿using Regule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regule.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ReguleDataContext db = new ReguleDataContext();
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            return View(db.Produtos.ToList());
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto Prod = db.Produtos.FirstOrDefault(x => x.Id == id);
+            if (Prod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Prod);
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Nome")] Produto Prod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produtos.InsertOnSubmit(Prod);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Prod);
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto Prod= db.Produtos.FirstOrDefault(x=>x.Id == id);
+            if (Prod== null)
+            {
+                return HttpNotFound();
+            }
+            return View(Prod);
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome")] Produto Prod)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produtos.FirstOrDefault(x=> x.Id == Prod.Id).Nome = Prod.Nome;
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Prod);
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto Prod= db.Produtos.FirstOrDefault(x=>x.Id == id);
+            if (Prod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Prod);
+        }
+        // POST: Produtos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto Prod = db.Produtos.FirstOrDefault(x=> x.Id ==id);
+            db.Produtos.DeleteOnSubmit(Prod);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Regule/Regule/Controllers/UnidadesController.cs
@@ -0,0 +1,122 @@
+﻿using Regule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regule.Controllers
+{
+    public class UnidadesController : Controller
+    {
+        private ReguleDataContext db = new ReguleDataContext();
+
+        // GET: Unidades
+        public ActionResult Index()
+        {
+            return View(db.Unidades.ToList());
+        }
+
+        // GET: Unidades/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade Unid = db.Unidades.FirstOrDefault(x => x.Id == id);
+            if (Unid == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Unid);
+        }
+
+        // GET: Unidades/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Unidades/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Sigla,Descricao")] Unidade Unid)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Unidades.InsertOnSubmit(Unid);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Unid);
+        }
+
+        // GET: Unidades/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade Unid = db.Unidades.FirstOrDefault(x => x.Id == id);
+            if (Unid == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Unid);
+        }
+
+        // POST: Unidades/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Sigla,Descricao")] Unidade Unid)
+        {
+            if (ModelState.IsValid)
+            {
+                Unidade tp = db.Unidades.FirstOrDefault(x => x.Id == Unid.Id);
+                tp.Descricao = Unid.Descricao;
+                tp.Sigla = Unid.Sigla;
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Unid);
+        }
+
+        // GET: Unidades/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Unidade Unid = db.Unidades.FirstOrDefault(x => x.Id == id);
+            if (Unid == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Unid);
+        }
+        // POST: Unidades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Unidade Unid = db.Unidades.FirstOrDefault(x => x.Id == id);
+            db.Unidades.DeleteOnSubmit(Unid);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }        
+    }
+}
