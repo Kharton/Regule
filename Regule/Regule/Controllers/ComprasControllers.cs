881347a1a92c83a28750a1777b386f86239a612b
Lucas Gabriel <lgfas1@hotmail.com>
2016-01-07 19:25:07 -0300

    Novos Crud's

---
+++ Regule/Regule/Controllers/ComprasControllers.cs
@@ -0,0 +1,127 @@
+﻿using Regule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regule.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private ReguleDataContext db = new ReguleDataContext();
+
+        // GET: Compras
+        public ActionResult Index()
+        {
+            return View(db.Compras.ToList());
+        }
+
+        // GET: Compras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compra Comp = db.Compras.FirstOrDefault(x => x.Id == id);
+            if (Comp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Comp);
+        }
+
+        // GET: Compras/Create
+        public ActionResult Create()
+        {
+            IEnumerable<Pessoa> pessoas = db.Pessoas.ToList();
+            ViewBag.Pessoas = pessoas.Select(h => new SelectListItem { Text = h.Nome, Value = h.Id.ToString()} );
+            return View();
+        }
+
+        // POST: Compras/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Data,Desconto,IdPessoa")] Compra Comp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Compras.InsertOnSubmit(Comp);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Comp);
+        }
+
+        // GET: Compras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compra Comp = db.Compras.FirstOrDefault(x => x.Id == id);
+            if (Comp == null)
+            {
+                return HttpNotFound();
+            }
+            IEnumerable<Pessoa> pessoas = db.Pessoas.ToList();
+            ViewBag.Pessoas = pessoas.Select(h => new SelectListItem { Text = h.Nome, Value = h.Id.ToString() });
+            return View(Comp);
+        }
+
+        // POST: Compras/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Desconto,Pessoa,Data")] Compra Comp)
+        {
+            if (ModelState.IsValid)
+            {
+                Compra tp = db.Compras.FirstOrDefault(x => x.Id == Comp.Id);
+                tp.Data = Comp.Data;
+                tp.Desconto = Comp.Desconto;
+                tp.IdPessoa = Comp.IdPessoa;
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Comp);
+        }
+
+        // GET: Compras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Compra Comp = db.Compras.FirstOrDefault(x => x.Id == id);
+            if (Comp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Comp);
+        }
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Compra Comp = db.Compras.FirstOrDefault(x => x.Id == id);
+            db.Compras.DeleteOnSubmit(Comp);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Regule/Regule/Controllers/VendasControllers.cs
@@ -0,0 +1,127 @@
+﻿using Regule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Regule.Controllers
+{
+    public class VendasController : Controller
+    {
+        private ReguleDataContext db = new ReguleDataContext();
+
+        // GET: Vendas
+        public ActionResult Index()
+        {
+            return View(db.Vendas.ToList());
+        }
+
+        // GET: Vendas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venda Vend = db.Vendas.FirstOrDefault(x => x.Id == id);
+            if (Vend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Vend);
+        }
+
+        // GET: Vendas/Create
+        public ActionResult Create()
+        {
+            IEnumerable<Pessoa> pessoas = db.Pessoas.ToList();
+            ViewBag.Pessoas = pessoas.Select(h => new SelectListItem { Text = h.Nome, Value = h.Id.ToString() });
+            return View();
+        }
+
+        // POST: Vendas/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Data,Desconto,Pessoa")] Venda Vend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Vendas.InsertOnSubmit(Vend);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Vend);
+        }
+
+        // GET: Vendas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venda Vend = db.Vendas.FirstOrDefault(x => x.Id == id);
+            if (Vend == null)
+            {
+                return HttpNotFound();
+            }
+            IEnumerable<Pessoa> pessoas = db.Pessoas.ToList();
+            ViewBag.Pessoas = pessoas.Select(h => new SelectListItem { Text = h.Nome, Value = h.Id.ToString() });
+            return View(Vend);
+        }
+
+        // POST: Vendas/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Data,Desconto,Pessoa")] Venda Vend)
+        {
+            if (ModelState.IsValid)
+            {
+                Venda tp = db.Vendas.FirstOrDefault(x => x.Id == Vend.Id);
+                tp.Data = Vend.Data;
+                tp.Desconto = Vend.Desconto;
+                tp.IdPessoa = Vend.IdPessoa;
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Vend);
+        }
+
+        // GET: Vendas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Venda Vend = db.Vendas.FirstOrDefault(x => x.Id == id);
+            if (Vend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Vend);
+        }
+        // POST: Vendas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Venda Vend = db.Vendas.FirstOrDefault(x => x.Id == id);
+            db.Vendas.DeleteOnSubmit(Vend);
+            db.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
